18ca56a857f3280567d81f5a66c9a059e7a6ee7c
Darryl Taylor <contact.dtaylor@gmail.com>
2021-11-20 13:27:50 -0500

    fix: footer update text

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,8 @@ import React from 'react'
 export const Footer = () => {
 	return (
 		<div className="text-center p-10 mt-10 border-t dark:border-gray-700 border-gray-200">
-			<h1>2021 Searchl, Inc.</h1>
+			<h1>2021 Gloggle, Inc.</h1>
+			<p className="text-gray-500 italic">Powered by Dawn Author Engineering</p>
 		</div>
 	)
 };
